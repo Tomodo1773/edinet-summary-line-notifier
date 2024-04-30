41a91e1d98b971c9635481bd4267498ad048ae31
pomo0508 <forever-career@hotmail.co.jp>
2024-04-30 22:30:52 +0900

    ウォッチリストをTomoStockから取得するように変更

--- functions/finance_summary.py
+++ functions/finance_summary.py
@@ -13,6 +13,7 @@ from utils.edinet import (
 )
 from utils.line import send_financial_summary
 from utils.llm import summarize_financial_reports
+from utils.get_watchlist import get_watchlist
 
 load_dotenv()
 
@@ -24,22 +25,24 @@ client = Client()
 
 def finance_summary():
     # ウォッチリスト銘柄のシンボルを登録しておく（ex. 8001: 伊藤忠商事）
-    watchlist = ["2163"]
+    # watchlist = ["2163"]
+    watchlist = get_watchlist()
+    logging.info(f"ウォッチリストを取得しました: {watchlist}")  # デバッグ用のコメント
 
     # 日付設定（本来は当日の日付を設定する想定）
-    # JST = timezone(timedelta(hours=+9))
-    # date = datetime.now(JST).strftime("%Y-%m-%d")
+    JST = timezone(timedelta(hours=+9))
+    date = datetime.now(JST).strftime("%Y-%m-%d")
 
     # 日付設定（テスト用に直近で伊藤忠の決算が出た日を設定）
     # date = "2023-06-22"
-    date = "2024-04-25"
+    # date = "2024-04-25"
 
     edinet_list = get_edinet_list(date)
     logging.info(f"{date}のEDINETリストを取得しました")  # デバッグ用のコメント
     watchlist_docs = filter_edinet_list(edinet_list, watchlist)
     if not watchlist_docs:
         logging.info("ウォッチリストの報告書はありませんでした。処理を終了します。")
-        exit()
+        return
     else:
         logging.info("ウォッチリストの報告書が新規アップロードされています。後続の処理を実行します。")
         # watchlist_docのfilerNameの値のみloggingで出力する
